dd5a65f6fc29517d2ae5def1bba4cd1144cd6495
Jeroen de Vries <jeroen@softwear.nl>
2023-09-24 09:30:36 +0200

    Improve deepCopy

--- src/tools.ts
+++ src/tools.ts
@@ -1,18 +1,18 @@
-import { MutexInterface } from "async-mutex"
+import { MutexInterface } from 'async-mutex'
 
 function deepCopy<Type>(obj: Type): Type {
-    return <Type>JSON.parse(JSON.stringify(obj))
+    return <Type>structuredClone(obj)
 }
 const EMPTY_COLLECTION = {
-    "checkPoint": 0,
-    "nextOpLogId": 1,
-    "savedAt": 0,
-    "lock": <MutexInterface>{},
-    "data": {},
-    "options": {},
-    "lastAccessed": 0,
-    "lastModified": 0,
-    "length":0
+    checkPoint: 0,
+    nextOpLogId: 1,
+    savedAt: 0,
+    lock: <MutexInterface>{},
+    data: {},
+    options: {},
+    lastAccessed: 0,
+    lastModified: 0,
+    length: 0,
 }
 /*
  * Ensure that an object is an array. If so, return object, else return a one-element array with the object
@@ -25,5 +25,5 @@ function ensureArray(object: any): Array<any> {
 export default {
     deepCopy,
     EMPTY_COLLECTION,
-    ensureArray
+    ensureArray,
 }
