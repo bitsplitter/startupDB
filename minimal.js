6a4e9a9b000fe3b17e99007fcbacca0b35ffade1
Jeroen de Vries <jeroen@softwear.nl>
2025-01-01 20:36:43 +0100

    Add console logging

--- minimal.js
+++ minimal.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const startupDB = require('./dist/server.js')
 
+const consoleLogger = function (req, res, next) {
+    console.log(req.url)
+    next()
+}
+
 const app = express()
 app.use(express.json())
+app.use(consoleLogger)
 app.use('/myDB', [startupDB.db({ dataFiles: './resources' })])
 app.listen(3000)
